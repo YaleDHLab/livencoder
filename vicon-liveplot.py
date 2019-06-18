2a9589f4b6cd259555684c9ea423c3042ae4e4b1
Chase Shimmin <chase.shimmin@yale.edu>
2019-06-18 17:21:14 -0400

    add some comments

--- vicon-liveplot.py
+++ vicon-liveplot.py
@@ -8,6 +8,7 @@ from mpl_toolkits.mplot3d.art3d import juggle_axes
 
 from keras.models import load_model
 
+# point labels, in the order assumed by the neural network model
 DEFAULT_POINT_LABELS = ['C7',
           'CLAV', 'LANK',
           'LBHD', 'LBSH',
@@ -37,11 +38,9 @@ DEFAULT_POINT_LABELS = ['C7',
           'STRN',
           'T10']
 
+# draw a 3d scatter plot
 def update3d(history, fig, ax, ranges, **kwargs):
     ax.cla()
-    #ax.set_xlim3d(-2000,2000)
-    #ax.set_ylim3d(-2000,2000)
-    #ax.set_zlim3d(0,1000)
     ax.set_xlim3d(*ranges[0])
     ax.set_ylim3d(*ranges[1])
     ax.set_zlim3d(*ranges[2])
@@ -49,6 +48,7 @@ def update3d(history, fig, ax, ranges, **kwargs):
     for x,a in zip(history, alphas):
         ax.scatter(x[:,0],x[:,1],x[:,2], alpha=a, **kwargs)
 
+# draw a 2d scatter plot
 def update2d(history, fig, ax, ranges, **kwargs):
     ax.cla()
     ax.set_xlim(*ranges[0])
@@ -58,30 +58,29 @@ def update2d(history, fig, ax, ranges, **kwargs):
         ax.scatter(x[:,0], x[:,1], alpha=a, **kwargs)
 
 if __name__ == "__main__":
-    n_skip = 4
+    # scale the framerate down by the given integer factor
+    # for plot updates.
+    framerate_prescale = 4
 
     current_frame = None
     current_subject_id = 0
     plt.ion()
 
     fig = plt.figure(figsize=(21,7))
+
+    # first axis, show the 3d point data for the mocap
     ax1 = fig.add_subplot(131, projection='3d')
-
-    #ax.set_xlim3d(-500,500)
-    #ax.set_ylim3d(400,1400)
-    #ax.set_zlim3d(0,1000)
-    #ax.hold(True)
-
     ax1_ranges = [(-0.5,0.5),(-0.5,0.5),(0,0.5)]
 
-    #ax2 = fig.add_subplot(132, projection='3d')
+    # second axis, show the 2d latent space
     ax2 = fig.add_subplot(132)
-    ax2_ranges = [(-2,2),(-2,2),(-2,2)]
+    ax2_ranges = [(-2,2),(-2,2)]
 
+    # third axis, show the orientation direction
     ax3 = fig.add_subplot(133)
-
     ax3_ranges = [(-1.2,1.2),(-1.2,1.2)]
 
+    # load in a pre-trained encoder network
     encoder = load_model('pose-enc-raymond.h5')
     encoder.summary()
 
@@ -96,6 +95,9 @@ if __name__ == "__main__":
 
     larm_history = []
     rarm_history = []
+    # read data from stdin, one line at a time.
+    # each frame's worth of data starts with a line "frame_number:NNN"
+    # and ends with the line "end_frame:NNN"
     while True:
         l = sys.stdin.readline()
         if not l:
@@ -115,7 +117,6 @@ if __name__ == "__main__":
         if k == 'n_subjects':
             current_frame['n_subjects'] = int(v)
             current_frame['subjects'] = [{} for _ in range(current_frame['n_subjects'])]
-            #assert current_frame['n_subjects'] == 1
         elif k == 'subject_id':
             current_subject_id = int(v)
         elif k == 'subject_name':
@@ -125,10 +126,13 @@ if __name__ == "__main__":
         elif k == 'coords':
             current_frame['subjects'][current_subject_id]['coords'] = np.float_(v.split(',')).reshape((-1,3))
         elif l.startswith('end_frame:'):
-            x = current_frame['subjects'][0]['coords']
-            #print(x.shape)
-            #print(current_frame)
+            # finished reading frame data from the input stream.
+            # now we can do something with it.
             iframe += 1
+
+            x = current_frame['subjects'][0]['coords']
+            # calculate forearm length to set the distance scale
+            # before feeding into network
             markers = current_frame['subjects'][0]['markers']
             lelb = markers.index('LELB')
             lowr = markers.index('LOWR')
@@ -140,22 +144,21 @@ if __name__ == "__main__":
             rarm_history.append(rarm)
             armlen = 0.5*(np.median(larm) + np.median(rarm))
             print("arm =",armlen)
-            if iframe % n_skip == 0:
+
+            # at the specified subsampling rate, update the figures
+            if iframe % framerate_prescale == 0:
                 ordering = [DEFAULT_POINT_LABELS.index(m) for m in current_frame['subjects'][0]['markers']]
                 x_prep = x[ordering] / armlen / 10.
                 x_prep[:,:2] -= x_prep[:,:2].mean(axis=-2, keepdims=True)
                 z,w = encoder.predict(np.expand_dims(x_prep[ordering],axis=0))
-                #w = np.array([np.cos(w), np.sin(w)])
                 print(z.shape)
                 history_x.append(x_prep)
                 history_x = history_x[-h_len_x:]
-                #history_z.append(z)
                 history_z.append(z[:,:2])
                 history_z = history_z[-h_len_z:]
                 history_w.append(w)
                 history_w = history_w[-h_len_w:]
                 update3d(history_x, fig, ax1, ax1_ranges, color='C0',s=10)
-                #update3d(history_z, fig, ax2, ax2_ranges, color='C1',s=70)
                 update2d(history_z, fig, ax2, ax2_ranges, color='C1',s=70)
                 update2d(history_w, fig, ax3, ax3_ranges, color='C2',s=300)
                 print(w)
