79aeb231dd5c783ba71ee4bca371dbc3914b87a1
duhaime <douglas.duhaime@gmail.com>
2019-06-21 07:50:03 -0400

    remove legacy model file

--- vicon-emulate.py
+++ vicon-emulate.py
@@ -3,51 +3,36 @@
 import numpy as np
 import time
 
-sleep = 0.0083 # 120 fps sleep
+sleep = 0.0165 # 120 fps sleep
 
-point_labels = ['C7',
-          'CLAV', 'LANK',
-          'LBHD', 'LBSH',
-          'LBWT', 'LELB',
-          'LFHD', 'LFRM',
-          'LFSH', 'LFWT',
-          'LHEL', 'LIEL',
-          'LIHAND', 'LIWR',
-          'LKNE', 'LKNI',
-          'LMT1', 'LMT5',
-          'LOHAND', 'LOWR',
-          'LSHN', 'LTHI',
-          'LTOE', 'LUPA',
-          'MBWT',
-          'MFWT', 'RANK',
-          'RBHD', 'RBSH',
-          'RBWT', 'RELB',
-          'RFHD', 'RFRM',
-          'RFSH', 'RFWT',
-          'RHEL', 'RIEL',
-          'RIHAND', 'RIWR',
-          'RKNE', 'RKNI',
-          'RMT1', 'RMT5',
-          'ROHAND', 'ROWR',
-          'RSHN', 'RTHI',
-          'RTOE', 'RUPA',
-          'STRN',
-          'T10']
+default_labels = [
+  'C7', 'CLAV', 'LANK', 'LBHD', 'LBSH',
+  'LBWT', 'LELB', 'LFHD', 'LFRM', 'LFSH',
+  'LFWT', 'LHEL', 'LIEL', 'LIHAND', 'LIWR',
+  'LKNE', 'LKNI', 'LMT1', 'LMT5', 'LOHAND',
+  'LOWR', 'LSHN', 'LTHI', 'LTOE', 'LUPA',
+  'MBWT', 'MFWT', 'RANK', 'RBHD', 'RBSH',
+  'RBWT', 'RELB', 'RFHD', 'RFRM', 'RFSH',
+  'RFWT', 'RHEL', 'RIEL', 'RIHAND', 'RIWR',
+  'RKNE', 'RKNI', 'RMT1', 'RMT5', 'ROHAND',
+  'ROWR', 'RSHN', 'RTHI', 'RTOE', 'RUPA',
+  'STRN', 'T10'
+]
 
-if __name__ == "__main__":
-    in_file = 'raymond-sample.npy'
-    d = np.load(in_file)
-    d = d[:,np.arange(d.shape[1])[1:]]
-    d = d[4200:]
+if __name__ == '__main__':
+  in_file = 'raymond-sample.npy'
+  d = np.load(in_file)
+  d = d[:,np.arange(d.shape[1])[1:]]
+  d = d[4200:]
 
-    while True:
-      for iframe in range(d.shape[0]):
-          time.sleep(sleep)
-          print("frame_number:%d"%iframe)
-          print("n_subjects:1")
-          print("subject_id:0")
-          print("subject_name:Raymond")
-          print("markers:%s"%",".join(point_labels))
-          print("coords:%s"%",".join(["%g"%x for x in d[iframe].reshape(-1)]))
-          print("end_frame:%d"%iframe)
-          print()
+  while True:
+    for iframe in range(d.shape[0]):
+      time.sleep(sleep)
+      print('frame_number:%d'%iframe)
+      print('n_subjects:1')
+      print('subject_id:0')
+      print('subject_name:Raymond')
+      print('markers:%s'%','.join(point_labels))
+      print('coords:%s'%','.join(['%g'%x for x in d[iframe].reshape(-1)]))
+      print('end_frame:%d'%iframe)
+      print()
